356ca14fdfc0644b45ee9c3756cabbd4cbb6f8f8
sfianchao1022 <sfianchao@yahoo.com.tw>
2020-08-06 22:45:15 +0800

    python_practice

---
+++ python_tutorial/app.py
@@ -0,0 +1,25 @@
+print('hello world')
+print('Ian')
+print('*'*10)
+
+# python會自動辨別型態
+price=10
+rating=0.9
+is_published=True
+name='Ian'
+print(price)
+print(rating)
+print(name)
+print(is_published)
+
+# scanf()
+# my_name=input('What is your name?')
+# print('hi '+my_name)
+
+birth_year=input('Birth year : ')
+print(type(birth_year))# 查看型態
+age=2019-int(birth_year)# int()轉換成integer
+print(type(age))
+print(age)
+
+
