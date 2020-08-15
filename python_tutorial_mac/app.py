09612aebf3b569b5d8bd05e4f93fb4f76127d9a9
sfianchao1022 <sfianchao@yahoo.com.tw>
2020-08-15 14:09:48 +0800

    .

--- python_tutorial_mac/app.py
+++ python_tutorial_mac/app.py
@@ -1,4 +1,4 @@
-course = 'python for "begineers"'
+course = 'Python for "Beginners"'
 course2 = "python's course for beginners"
 email = '''
 hi Ian,
@@ -21,6 +21,29 @@ print("last character : "+course[-1])  # 從開頭-1 回到字串最後一位字
 print(course[-3])
 name = "jennifer"
 print(name[1:-1])  # 從最後一個-1
+
+firstName = 'Ian'
+lastName = 'Chao'
+# formatted Strings f'...{variable}...' 用大括弧包住變數可以直接寫在string內
+print(firstName + ' [' + lastName + '] is a coder')
+msg = f'{firstName} [{lastName}] is a coder'
+print(msg)
+
+courseName = 'python for beginners'
+print(f'my course name is " {courseName} "')
+print(type(len(courseName)))  # len()
+print(len(courseName))
+print(courseName.upper())  # upper()
+print(courseName.lower())  # lower()
+
+print(courseName.find('p'))  # find char 'p' at index 0
+print(type(courseName.find('f')))
+print(courseName.find('x'))  # can not find char 'x' in the string and return -1
+print(courseName.find('beginners'))  # find 'beginners' start at index 11
+print(courseName.replace('beginners', 'absolute beginners'))
+print('python' in courseName)  # return boolean
+
+
 
 
 
